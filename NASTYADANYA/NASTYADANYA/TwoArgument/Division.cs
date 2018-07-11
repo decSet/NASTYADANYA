ac42cc9373fc766288c164218c1c46fcd6678cc3
decSet <40753743+decSet@users.noreply.github.com>
2018-07-12 00:51:29 +0600

    Add Division by zero

--- NASTYADANYA/NASTYADANYA/TwoArgument/Division.cs
+++ NASTYADANYA/NASTYADANYA/TwoArgument/Division.cs
@@ -1,9 +1,17 @@
-﻿namespace NASTYADANYA
+﻿namespace NASTYADANYA;
+using System;
 {
     public class Division : ITwoArgumentsCalculator
     {
         public double Calculate(double firstArgument, double secondArgument)
         {
+            {
+                            if (secondArgument == 0)
+                                {
+                    throw new Exception("Error! Division by zero!");
+                                }
+                return firstArgument / secondArgument;
+            }
             return firstArgument / secondArgument;
         }
     }
